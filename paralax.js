6a2cf2d866d6d052fdb9cd3300f0f58bee72b56c
Dmitriy Vinogradov <di.vinogradov@gmail.com>
2021-12-16 13:12:10 +0300

    Paralax with perspective

--- paralax.js
+++ paralax.js
@@ -5,6 +5,7 @@ function Parallax(options) {
     wrapper: options.wrapper || '.parallax',                // string: wrapper class name
     layers: options.layers || '.parallax-layer',            // string: layer class name
     deep: options.deep || 'data-parallax-deep',             // number: -5000 ... 5000
+    origin: options.origin || 'data-parallax-origin',           // number: 0 ... width
     disallow: options.disallow || 'data-parallax-disallow', // string: 'x' || 'y' || 'both'
   }
 
@@ -19,17 +20,24 @@ function Parallax(options) {
   const getTranslateFuction = (wrapper) => {
     const self = this
     return (event) => {
-      const { clientX: x, clientY: y } = event
+      const { clientX, clientY} = event
+      const x = 100 * clientX / wrapper.offsetWidth
+      const y = 100 * clientY / wrapper.offsetHeight
       const layers = getLayers(wrapper)
       for (let layer of layers) {
         const disallow = layer.getAttribute(self.names.disallow)
+        const origin = layer.getAttribute(self.names.origin)
         if (disallow && disallow === 'both') return
 
         const deep = layer.getAttribute(self.names.deep)
         const itemX = (disallow && disallow === 'x') ? 0 : x / deep
-        const itemY = (disallow && disallow === 'y') ? 0 : y * 5 / deep
+        const itemY = (disallow && disallow === 'y') ? 0 : y * 2 / deep
 
         layer.style.transform = `translateX(${itemX}%) translateY(${itemY}%)`
+        if(origin) {
+          layer.style['transform-origin'] = origin
+          layer.style.transform += ` rotateY(${5+itemX*2}deg) rotateX(${5+itemX*1.8}deg)`
+        }
       }
     }
   }
