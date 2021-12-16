7acdb1c5ede36b2607c81c3408d8c529df0227ca
Dmitriy Vinogradov <di.vinogradov@gmail.com>
2021-12-16 14:34:46 +0300

    Added rotation to parallax

--- paralax.js
+++ paralax.js
@@ -2,11 +2,13 @@ function Parallax(options) {
   // Defines how classes and attributes are named 
   options = options || {}
   this.names = {
-    wrapper: options.wrapper || '.parallax',                // string: wrapper class name
-    layers: options.layers || '.parallax-layer',            // string: layer class name
-    deep: options.deep || 'data-parallax-deep',             // number: -5000 ... 5000
-    origin: options.origin || 'data-parallax-origin',           // number: 0 ... width
-    disallow: options.disallow || 'data-parallax-disallow', // string: 'x' || 'y' || 'both'
+    wrapper: options.wrapper || '.parallax',                              // string: wrapper class name
+    layers: options.layers || '.parallax-layer',                          // string: layer class name
+    deep: options.deep || 'data-parallax-deep',                           // number: -5000...5000
+    origin: options.origin || 'data-parallax-origin',                     // number: 0...width
+    perspectiveX: options.perspectiveX || 'data-parallax-perspectiveX',   // float: 0...5, perspective coefficient
+    perspectiveY: options.perspectiveY || 'data-parallax-perspectiveY',   // float: 0...5, perspective coefficient
+    disallow: options.disallow || 'data-parallax-disallow',               // string: 'x' || 'y' || 'both'
   }
 
   // Return collection of parallax wrapers
@@ -27,16 +29,18 @@ function Parallax(options) {
       for (let layer of layers) {
         const disallow = layer.getAttribute(self.names.disallow)
         const origin = layer.getAttribute(self.names.origin)
+        const perspectiveX = layer.getAttribute(self.names.perspectiveX)
+        const perspectiveY = layer.getAttribute(self.names.perspectiveY)
         if (disallow && disallow === 'both') return
 
         const deep = layer.getAttribute(self.names.deep)
         const itemX = (disallow && disallow === 'x') ? 0 : x / deep
-        const itemY = (disallow && disallow === 'y') ? 0 : y * 2 / deep
+        const itemY = (disallow && disallow === 'y') ? 0 : y / deep
 
         layer.style.transform = `translateX(${itemX}%) translateY(${itemY}%)`
-        if(origin) {
+        if(origin && perspectiveX && perspectiveY) {
           layer.style['transform-origin'] = origin
-          layer.style.transform += ` rotateY(${5+itemX*2}deg) rotateX(${5+itemX*1.8}deg)`
+          layer.style.transform += ` rotateY(${5+itemX*perspectiveX}deg) rotateX(${15-(itemY*perspectiveY)}deg) rotateZ(${itemX*(-perspectiveY/10)}deg)`
         }
       }
     }
