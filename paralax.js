43952c63cadf0a77e7a30fae2c067c4e05976b2f
Dmitriy Vinogradov <di.vinogradov@gmail.com>
2021-12-16 11:38:00 +0300

    Refactored js & scss

--- paralax.js
+++ paralax.js
@@ -1,76 +1,51 @@
 function Parallax(options) {
-    options = options || {};
-    this.nameSpaces = {
-        wrapper: options.wrapper || '.parallax',
-        layers: options.layers || '.parallax-layer',
-        deep: options.deep || 'data-parallax-deep'
-    };
-    this.init = function() {
-        var self = this,
-            parallaxWrappers = document.querySelectorAll(this.nameSpaces.wrapper);
-        for (var i = 0; i < parallaxWrappers.length; i++) {
-            (function(i) {
-                parallaxWrappers[i].addEventListener('mousemove', function(e) {
-                    var x = e.clientX,
-                        y = e.clientY,
-                        layers = parallaxWrappers[i].querySelectorAll(self.nameSpaces.layers);
-                    for (var j = 0; j < layers.length; j++) {
-                        (function(j) {
-                            var deep = layers[j].getAttribute(self.nameSpaces.deep),
-                                disallow = layers[j].getAttribute('data-parallax-disallow'),
-                                itemX = (disallow && disallow === 'x') ? -50 : x / deep - 1,
-                                itemY = (disallow && disallow === 'y') ? 0 : y / deep;
-                            if (disallow && disallow === 'both') return;
-                            layers[j].style.transform = 'translateX(' + itemX + '%) translateY(' + itemY + '%)';
-                        })(j);
-                    }
-                })
-            })(i);
-        }
-    };
-    this.init();
-    return this;
+  // Defines how classes and attributes are named 
+  options = options || {}
+  this.names = {
+    wrapper: options.wrapper || '.parallax',                // string: wrapper class name
+    layers: options.layers || '.parallax-layer',            // string: layer class name
+    deep: options.deep || 'data-parallax-deep',             // number: -5000 ... 5000
+    disallow: options.disallow || 'data-parallax-disallow', // string: 'x' || 'y' || 'both'
+  }
+
+  // Return collection of parallax wrapers
+  const getWrappers = () => document.querySelectorAll(this.names.wrapper)
+
+  // Return parallax layers for given wrapper
+  const getLayers = (wrapper) => wrapper.querySelectorAll(this.names.layers)
+
+  // Return a callback function for `mousemove` event, 
+  // which changes the transform attribute in the layer style
+  const getTranslateFuction = (wrapper) => {
+    const self = this
+    return (event) => {
+      const { clientX: x, clientY: y } = event
+      const layers = getLayers(wrapper)
+      for (let layer of layers) {
+        const disallow = layer.getAttribute(self.names.disallow)
+        if (disallow && disallow === 'both') return
+
+        const deep = layer.getAttribute(self.names.deep)
+        const itemX = (disallow && disallow === 'x') ? 0 : x / deep
+        const itemY = (disallow && disallow === 'y') ? 0 : y * 5 / deep
+
+        layer.style.transform = `translateX(${itemX}%) translateY(${itemY}%)`
+      }
+    }
+  }
+
+  // Initialization of event listener for all paralax wrappers
+  this.init = function () {
+    const wrappers = getWrappers()
+    for (let wrapper of wrappers) {
+      wrapper.addEventListener('mousemove', getTranslateFuction(wrapper))
+    }
+  }
+
+  this.init()
+  return this
 }
 
-window.addEventListener('load', function() {
-    new Parallax();
-});
-
-
-//socials
-let items = document.querySelectorAll(".socials-item-icon"),
-    self = this;
-items.forEach((item, index) => {
-    item.addEventListener("mousemove", mouseMove);
-    item.addEventListener("mouseleave", mouseLeave);
-});
-
-function mouseMove(e) {
-    let target = e.target.closest("a"),
-        targetData = target.getBoundingClientRect(),
-        targetIcon = target.querySelector("i"),
-        offset = {
-            x: ((e.pageX - (targetData.left + targetData.width / 2)) / 4) * -1,
-            y: ((e.pageY - (targetData.top + targetData.height / 2)) / 4) * -1
-        };
-    target.style.transform = "translate(" + offset.x + "px," + offset.y + "px) scale(" + 1.1 + ")";
-    target.style.webkitTransform = "translate(" + offset.x + "px," + offset.y + "px) scale(" + 1.1 + ")";
-    document.querySelectorAll(".socials-item-icon").forEach((e) => {
-        if (e !== target) {
-            e.style.transform = "translate(" + offset.x / 2 + "px, " + offset.y / 2 + "px) scale(" + 0.9 + ")";
-            e.style.webkitTransform = "translate(" + offset.x / 2 + "px, " + offset.y / 2 + "px) scale(" + 0.9 + ")";
-        }
-    });
-    targetIcon.style.transform = "translate(" + offset.x + "px," + offset.y + "px) scale(" + 1.1 + ")";
-    targetIcon.style.webkitTransform = "translate(" + offset.x + "px," + offset.y + "px) scale(" + 1.1 + ")";
-}
-
-function mouseLeave(e) {
-    document.querySelectorAll(".socials-item-icon").forEach((target) => {
-        let targetIcon = target.querySelector("i");
-        target.style.transform = "translate(0px,0px) scale(1)";
-        target.style.webkitTransform = "translate(0px,0px) scale(1)";
-        targetIcon.style.transform = "translate(0px,0px) scale(1)";
-        targetIcon.style.webkitTransform = "translate(0px,0px) scale(1)";
-    });
-}
+window.addEventListener('load', function () {
+  new Parallax()
+})
